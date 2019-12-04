4861ca5783619d6f02761aa2daf17b54cdb09dfc
brunorech <brunogrech@hotmail.com>
2019-12-04 17:03:15 -0300

    Basic author crud working

--- src/controllers/author/AuthorController.js
+++ src/controllers/author/AuthorController.js
@@ -2,11 +2,19 @@ const Author = require('../../models/Author');
 
 module.exports = {
     async index(req, res) {
-
+        const author = await Author.findAll();
+        res.json(author);
     },
 
     async show(req, res) {
+        const { author_id } = req.params;
+        const author = await Author.findByPk(author_id);
 
+        if (!author) {
+            return res.status(400).json({ message: 'Author not found' });
+        }
+
+        res.json(author);
     },
 
     async store(req, res) {
@@ -16,10 +24,34 @@ module.exports = {
     },
 
     async update(req, res) {
+        const { author_id } = req.params;
+        const { name } = req.body;
+        const author = await Author.findByPk(author_id);
 
+        if (!name) {
+            return res.status(400).json({ message: 'Invalid name' });
+        }
+        if (!author) {
+            return res.status(400).json({ message: 'Author not found' });
+        }
+
+        await Author.update({ name }, {
+            where: {
+                id: author_id
+            }
+        });
+
+        res.send();
     },
 
     async destroy(req, res) {
+        const { author_id } = req.params;
+        await Author.destroy({
+            where: {
+                id: author_id,
+            }
+        });
 
+        return res.send();
     },
 }
