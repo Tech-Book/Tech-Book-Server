3a5f2acc5cc89407da9fa2044c78a82370797d5e
brunorech <brunogrech@hotmail.com>
2019-12-04 14:42:09 -0300

    Basic server running

---
+++ src/Server.js
@@ -0,0 +1,10 @@
+// require('./database');
+const express = require('express');
+const routes = require('./Routes');
+
+const server = express();
+
+server.use(express.json());
+server.use(routes);
+
+server.listen(3000);
