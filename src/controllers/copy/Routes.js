5435e2f1b0a431f9bfddc13b2452b4da7f30538e
brunorech <brunogrech@hotmail.com>
2019-12-06 16:12:02 -0300

    Book Copy validator added

--- src/controllers/copy/Routes.js
+++ src/controllers/copy/Routes.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const validate = require('express-validation');
+const handle = require('express-async-handler');
 const CopyController = require('./CopyController');
+const CopyValidator = require('../../validators/copy/CopyValidator');
 
 const routes = express.Router();
 
 routes.get('/', CopyController.index);
-routes.post('/', CopyController.store);
+routes.post('/', validate(CopyValidator), handle(CopyController.store));
 routes.get('/:copy_id', CopyController.show);
 routes.delete('/:copy_id', CopyController.destroy);
 
