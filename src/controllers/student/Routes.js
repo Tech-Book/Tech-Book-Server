9210de1c70109522d0c9c1469ac971a0ae5e250b
brunorech <brunogrech@hotmail.com>
2019-12-06 16:18:17 -0300

    Student validator added

--- src/controllers/student/Routes.js
+++ src/controllers/student/Routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const validate = require('express-validation');
+const handle = require('express-async-handler');
 const StudentController = require('./StudentController');
+const StudentValidator = require('../../validators/student/StudentValidator');
 
 const routes = express.Router();
 
 routes.get('/', StudentController.index);
-routes.post('/', StudentController.store);
+routes.post('/', validate(StudentValidator), handle(StudentController.store));
 routes.get('/:student_id', StudentController.show);
-routes.put('/:student_id', StudentController.update);
+routes.put('/:student_id', validate(StudentValidator), handle(StudentController.update));
 routes.delete('/:student_id', StudentController.destroy);
 
 module.exports = routes;
