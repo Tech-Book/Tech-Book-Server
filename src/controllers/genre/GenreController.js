ecf915530754a2e2f79f5ed5ad6c99d189beb423
brunorech <brunogrech@hotmail.com>
2019-12-06 15:40:35 -0300

    Genre validator added

--- src/controllers/genre/GenreController.js
+++ src/controllers/genre/GenreController.js
@@ -17,11 +17,9 @@ module.exports = {
         try {
             const { genre_id } = req.params;
             const genre = await Genre.findByPk(genre_id);
-
             if (!genre) {
                 return res.status(400).json({ message: 'Genre not found' });
             }
-
             res.json(genre);
         } catch (error) {
             res.status(500).json({
@@ -48,14 +46,6 @@ module.exports = {
         try {
             const { genre_id } = req.params;
             const { name } = req.body;
-            const genre = await Genre.findByPk(genre_id);
-
-            if (!name) {
-                return res.status(400).json({ message: 'Invalid name' });
-            }
-            if (!genre) {
-                return res.status(400).json({ message: 'Genre not found' });
-            }
 
             await Genre.update({ name }, {
                 where: {
--- src/controllers/genre/Routes.js
+++ src/controllers/genre/Routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const validate = require('express-validation');
+const handle = require('express-async-handler');
 const GenreController = require('./GenreController');
+const GenreValidator = require('../../validators/Genre/GenreValidator');
 
 const routes = express.Router();
 
 routes.get('/', GenreController.index);
-routes.post('/', GenreController.store);
+routes.post('/', validate(GenreValidator), handle(GenreController.store));
 routes.get('/:genre_id', GenreController.show);
-routes.put('/:genre_id', GenreController.update);
+routes.put('/:genre_id', validate(GenreValidator), handle(GenreController.update));
 routes.delete('/:genre_id', GenreController.destroy);
 
 module.exports = routes;
